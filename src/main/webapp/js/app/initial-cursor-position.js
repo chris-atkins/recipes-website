450b8501daba9c519ec913de9b6836df0299a4c8
Chris <chrisatkins55@gmail.com>
2015-02-08 23:00:32 -0500

    adding js for giving focus to an element

--- src/main/webapp/js/app/initial-cursor-position.js
+++ src/main/webapp/js/app/initial-cursor-position.js
@@ -1,5 +1,13 @@
-(function ($) {
+var initialCursorPositionNS = {};
+(function($, ns) {
 
-	
+	ns.init = function() {
+		var wantingFocus = $('.start-with-focus');
+		if (wantingFocus) {
+			wantingFocus[0].focus();
+		}
+	};
 
-})(jQuery);
+	ns.init();
+
+})(jQuery, initialCursorPositionNS);
--- src/test/webapp/js/specs/initial-cursor-position-spec.js
+++ src/test/webapp/js/specs/initial-cursor-position-spec.js
@@ -1,7 +1,26 @@
 describe("The initial-cursor-position.js... ", function() {
   
-	it("sets the focus on an html element that has the 'start-with-focus' class.", function() {
+	describe("when there is one element on the screen with the 'start-with-focus' class...", function() {
+
+		beforeEach(function() {
+			affix('input[value="there"]+input.start-with-focus[value="hi"]');
+			initialCursorPositionNS.init();
+		});
 		
+		it("that element has focus when the page is loaded.", function() {
+			expect($('.start-with-focus')).toBeFocused();
+		});
 	});
 	
+	describe("when there are multiple elements on the screen with the 'start-with-focus' class...", function() {
+		
+		beforeEach(function() {
+			affix('input.start-with-focus#id1[value="hi"]+input.start-with-focus#id2[value="there"]');
+			initialCursorPositionNS.init();
+		});
+		
+		it("the first element has focus when the page is loaded.", function() {
+			expect($('#id1')).toBeFocused();
+		});
+	});
 });
