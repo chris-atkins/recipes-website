7af7de6993f0098ad03bee4c0b6e8b2fe4970deb
Chris Atkins <chrisatkins55@gmail.com>
2016-03-16 13:48:52 -0400

    updating sql file so it is runnable from cli - mysql does not like -- as a comment

--- database/C01_RECIPE/1-add-searchableText-column.sql
+++ database/C01_RECIPE/1-add-searchableText-column.sql
@@ -2,8 +2,8 @@ ALTER TABLE C01_RECIPE ADD COLUMN C01_SEARCHABLE_TEXT TEXT NULL AFTER C01_RECIPE
 
 UPDATE C01_RECIPE SET C01_LAST_UPDATED_ON = CURRENT_TIMESTAMP() WHERE C01_RECIPE_ID < 100 AND C01_LAST_UPDATED_ON IS NULL;
 
---*****************************************************************************************
--- now must run app initializer that fills out the column before running the next statement
---*****************************************************************************************
+/*****************************************************************************************
+ now must run app initializer that fills out the column before running the next statement
+*****************************************************************************************/
 
 ALTER TABLE C01_RECIPE MODIFY C01_SEARCHABLE_TEXT TEXT NOT NULL;
